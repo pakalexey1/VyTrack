67a302a9e8132ef0edd84c802ae6c4ae362a7211
netta <nettastor@gmail.com>
2022-06-30 12:54:54 -0400

    It passes for one person so far- in order to pass it for everybody - u ll need to have a loop to go through each of 9 users and also log out each time or open 9 windows in chrome browser. For now i put tag "@StoreManager" to run

--- src/test/java/com/cydeo/step_definitions/ExportGridDropdown.java
+++ src/test/java/com/cydeo/step_definitions/ExportGridDropdown.java
@@ -2,12 +2,16 @@ package com.cydeo.step_definitions;
 
 import com.cydeo.pages.VehiclesPage;
 import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
 import com.cydeo.utilities.VyTrackUtils;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ExportGridDropdown {
 
@@ -19,6 +23,8 @@ public class ExportGridDropdown {
     @When("user clicks on Export Grid dropdown button")
     public void user_clicks_on_export_grid_dropdown_button() {
         VehiclesPage vehiclesPage = new VehiclesPage();
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),30);
+        wait.until(ExpectedConditions.elementToBeClickable(vehiclesPage.exportGridDropdownButton));
         vehiclesPage.exportGridDropdownButton.click();
     }
     @Then("user should see two options: CSV and XLSX")
