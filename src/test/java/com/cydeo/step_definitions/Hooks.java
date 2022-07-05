c38b2825e73df8c1dac17c0beab459fde05d0c25
netta <nettastor@gmail.com>
2022-07-05 11:53:53 -0400

    Hooks

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -1,25 +1,21 @@
 package com.cydeo.step_definitions;
-
-
 import com.cydeo.utilities.Driver;
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import org.junit.After;
+import org.junit.AfterClass;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-
 public class Hooks {
+       // @Before
+        //public void setUpScenario(){}
+@After
+        public void teardownScenario(Scenario scenario){
+            System.out.println("--> It is coming from @After in Hooks ");
+            byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png",scenario.getName());
+            Driver.closeDriver();}
+@AfterClass
+    public void close_window(){
+    Driver.closeDriver();
+}}
 
-    @Before
-    public void setUpScenario(){
-
-    }
-    @After
-    public void tearDownScenario(Scenario scenario){
-
-        byte [] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
-        scenario.attach(screenshot, "image/png", scenario.getName());
-
-        Driver.closeDriver();
-    }
-}
