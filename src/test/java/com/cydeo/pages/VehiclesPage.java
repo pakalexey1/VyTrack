3afab4d6749d8d37e73069af4cd910c2c504787c
AlexaSky <alexasky111@gmail.com>
2022-07-05 13:02:01 -0400

    adding ifButtonOnTheLeft() method for feature 6

--- src/test/java/com/cydeo/pages/VehiclesPage.java
+++ src/test/java/com/cydeo/pages/VehiclesPage.java
@@ -1,6 +1,7 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -20,7 +21,7 @@ public class VehiclesPage {
     @FindBy(css = "a[title='CSV']")
     public WebElement csvButton;
 
-    @FindBy(css="a[title='XLSX']")
+    @FindBy(css = "a[title='XLSX']")
     public WebElement xlsxButton;
     @FindBy(css="a[title='Grid Settings']")
     public WebElement grid_settings;
@@ -35,19 +36,19 @@ public class VehiclesPage {
     @FindBy(css = "li[class='last']>a[class='no-hash']")
     public WebElement logoutButton;
 
-    @FindBy (className = "oro-subtitle")
+    @FindBy(className = "oro-subtitle")
     public WebElement allCarsDropDownButton;
 
-    @FindBy (css = "a[data-value=\"__all__\"]")
+    @FindBy(css = "a[data-value=\"__all__\"]")
     public WebElement allCarsOption;
 
-    @FindBy (css = "a[title=\"Refresh\"]")
+    @FindBy(css = "a[title=\"Refresh\"]")
     public WebElement refreshButton;
 
     @FindBy(xpath = "//a[@title='Reset']")
     public WebElement resetButton;
 
-    public void logout(){
+    public void logout() {
         wait.until(ExpectedConditions.visibilityOf(this.userMenuButton));
         this.userMenuButton.click();
 
@@ -59,4 +60,14 @@ public class VehiclesPage {
 
     @FindBy(css = "div[class='column-manager dropdown open']")
     public WebElement gridSettingsDropdown;
+
+    public boolean ifButtonOnTheLeft(String buttonOne, String buttonTwo) {
+        boolean check = false;
+        String xpath = "//a[@title='" + buttonOne + "']/..//preceding-sibling::a[@title='" + buttonTwo + "']";
+        WebElement element = Driver.getDriver().findElement(By.xpath(xpath));
+        if (element.isDisplayed()) {
+            check = true;
+        }
+        return check;
+    }
 }
