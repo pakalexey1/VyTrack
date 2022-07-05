19db181672aac1e282abb42911453a529a796728
netta <nettastor@gmail.com>
2022-07-05 11:57:44 -0400

    Json run

--- src/test/java/com/cydeo/runner/TestRunJson.java
+++ src/test/java/com/cydeo/runner/TestRunJson.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
         features="src/test/resources/features",
         glue="com/cydeo/step_definitions",
         dryRun = false,
-        tags="@Test"
+        tags="@Hanna"
 )
 public class TestRunJson { }
+//added json runner
