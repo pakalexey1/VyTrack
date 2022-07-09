5c33346a18128414a4aedcd35bf54919695da80f
Viktoriia Mishchenko <viktoriiamish@gmail.com>
2022-07-09 19:49:51 -0400

    refactoring

--- src/test/java/com/cydeo/utilities/VyTrackUtils.java
+++ src/test/java/com/cydeo/utilities/VyTrackUtils.java
@@ -2,15 +2,14 @@ package com.cydeo.utilities;
 
 import com.cydeo.pages.DashboardPage;
 import com.cydeo.pages.LoginPage;
-import com.cydeo.pages.BasePage;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class VyTrackUtils {
+
     public static void login(String username, String password) {
         Driver.getDriver().get(ConfigurationReader.getProperty("vytrack.login"));
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 60);
 
         LoginPage loginPage = new LoginPage();
         loginPage.usernameBox.sendKeys(ConfigurationReader.getProperty(username));
@@ -30,10 +29,17 @@ public class VyTrackUtils {
         dashboardPage.fleetModule.click();
         dashboardPage.vehiclesOption.click();
     }
-    public static void vyTrack_logout() {
-        BasePage logoutFunction = new BasePage();
-        logoutFunction.logout_dropdown.click();
-        logoutFunction.logout_button.click();
-    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
 
