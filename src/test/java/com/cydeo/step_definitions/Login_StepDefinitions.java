b84eb748c342e7833cdb29626468b0bb66aa10b3
Viktoriia Mishchenko <viktoriiamish@gmail.com>
2022-07-09 19:48:58 -0400

    refactoring

--- src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
@@ -2,8 +2,10 @@ package com.cydeo.step_definitions;
 
 import com.cydeo.pages.BasePage;
 import com.cydeo.pages.LoginPage;
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.Driver;
+import com.cydeo.utilities.VyTrackUtils;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -15,7 +17,7 @@ public class Login_StepDefinitions {
 
     BasePage basePage = new BasePage();
     LoginPage loginPage = new LoginPage();
-    WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+
 
     @Given("user is on the VyTrack login page")
     public void user_is_on_the_vy_track_login_page() {
@@ -29,7 +31,7 @@ public class Login_StepDefinitions {
 
     @Then("user should see dashboard")
     public void user_should_see_dashboard() {
-        wait.until(ExpectedConditions.titleContains(ConfigurationReader.getProperty("dashboard")));
+        BrowserUtils.getWait10().until(ExpectedConditions.titleContains(ConfigurationReader.getProperty("dashboard")));
         Assert.assertEquals("Dashboard verification failed", ConfigurationReader.getProperty("dashboard"), Driver.getDriver().getTitle() );
         basePage.logout();
     }
