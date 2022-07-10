16b0b1be0bbd3bd04db1a0815c83b7a5eab25ee3
Viktoriia Mishchenko <viktoriiamish@gmail.com>
2022-07-09 22:27:04 -0400

    refactoring

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -3,12 +3,9 @@ import com.cydeo.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
-import org.junit.AfterClass;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 public class Hooks {
-
-
     @Before
     public void setUpScenario(){
 
@@ -16,8 +13,6 @@ public class Hooks {
 
     @After
     public void teardownScenario(Scenario scenario){
-        System.out.println("--> It is coming from @After in Hooks ");
-
 
         if(scenario.isFailed()) {
 
