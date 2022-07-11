8a31ccff2d22b15dba2e71b185ce3003e6a80f07
Viktoriia Mishchenko <viktoriiamish@gmail.com>
2022-07-11 13:56:40 -0400

    refactoring

--- src/test/java/com/cydeo/pages/BasePage.java
+++ src/test/java/com/cydeo/pages/BasePage.java
@@ -27,6 +27,7 @@ public class BasePage extends BaseModel{
 
     public void logout() {
 
+        getWait10().until(ExpectedConditions.visibilityOf(logout_dropdown));
         getWait10().until(ExpectedConditions.elementToBeClickable(logout_dropdown));
         logout_dropdown.click();
 
